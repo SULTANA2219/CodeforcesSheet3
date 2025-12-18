e93d2f17c037f524c86fac1bb8890c333ad3e999
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-12-18 16:53:54 +0600

    Create pH.cpp

---
+++ pH.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    // Bubble Sort
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                swap(A[j], A[j + 1]);
+            }
+        }
+    }
+
+    // Print result
+    for (int i = 0; i < N; i++) {
+        cout << A[i];
+        if (i < N - 1) cout << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
