a60562f4c6f2069163d82cb8bb63d18b03a78b70
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-12-18 17:05:59 +0600

    Update pX.cpp

--- pX.cpp
+++ pX.cpp
@@ -4,16 +4,23 @@ using namespace std;
 int main() {
     int N, M;
     cin >> N >> M;
+
     vector<vector<char>> A(N, vector<char>(M));
 
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < M; j++)
-            cin >> A[i][j];
+    // Read the matrix
+    for (int i = 0; i < N; i++) {
+        string row;
+        cin >> row;
+        for (int j = 0; j < M; j++) {
+            A[i][j] = row[j]; // store each character
+        }
+    }
 
     int X, Y;
     cin >> X >> Y;
     int r = X - 1, c = Y - 1; // convert to 0-indexed
 
+    // 8 directions: top-left, top, top-right, left, right, bottom-left, bottom, bottom-right
     int dx[] = {-1,-1,-1,0,0,1,1,1};
     int dy[] = {-1,0,1,-1,1,-1,0,1};
 
@@ -23,13 +30,13 @@ int main() {
         int nr = r + dx[i];
         int nc = c + dy[i];
         if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
-            if (A[nr][nc] != 'X') {
+            if (A[nr][nc] != 'x' && A[nr][nc] != 'X') { // consider both 'x' and 'X'
                 allX = false;
                 break;
             }
         }
     }
 
-    cout << (allX ? "yes" : "no") << "\n";
+    cout << (allX ? "yes" : "no") << endl;
     return 0;
 }
